97848dfe51011c618b71176653ce46c134bd8476
Tijuana22 <tijuanahunt39@gmail.com>
2023-02-15 23:34:59 -0500

    added some css

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -4,15 +4,18 @@ const RecipeCard= ({recipe}) => {
     const {idMeal, strMeal, strCategory, strMealThumb} = recipe;
   return (
     <div className="card">
-    <img
-    src={strMealThumb}
-    alt={strMeal}
+    <img src={strMealThumb} alt={strMeal}
     className='card-image' />
+
+    
+
     <div className='card-body'>
         <span className='category'>{strCategory}</span>
-        <a href={"https://www.themealdb.com/meal/" +idMeal} rel="noreferrer"target= "_blank">Ingredients</a>
 
         <h3>{strMeal}</h3>
+
+        <a href={"https://www.themealdb.com/meal/" +idMeal} rel="noreferrer"target= "_blank"> Ingredients </a>
+
         
     </div>
     </div>
